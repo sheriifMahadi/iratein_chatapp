a0df08c58e938f5deb9f92803b21f55397e96825
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-01-17 10:40:07 +0100

    create message models and persist messages

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,3 +1,18 @@
+from sqlite3 import Timestamp
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sender_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self) -> str:
+        return self.sender.username
+
+    def load_previous_messages(self, messages=10):
+        return Message.objects.order_by('-timestamp').all()[:messages]
