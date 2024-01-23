dbb8c4caa8dcc0d92b49eab43a712a8b4e86f232
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-01-23 21:23:05 +0100

    refactor code and update consumer.py methods

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,86 +1,53 @@
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
 from .models import Message
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-
-class ChatConsumer(WebsocketConsumer):
-
-    def fetch_messages(self, data):
-        messages = Message.load_previous_messages(10)
-        content = {
-            'messages': self.parse_to_json(messages)
-        }
-        self.send_chat_message(content)
-
-    def new_message(self, data):
-        sender = data['from']
-        sender_user = User.objects.filter(username=sender)[0]
-        message = Message.objects.create(sender=sender_user, content=data['message'])
-        content = {
-            'command': 'new_message',
-            'message': self.parse_single_to_json(message)
-        }
-        return self.send_chat_message(content)
-
-    def parse_to_json(self, messages):
-        parsed = []
-        for message in messages:
-            parsed.append(self.parse_single_to_json(message))
-        return parsed
-
-    def parse_single_to_json(self, message):
-        return {
-            'sender': message.sender.username,
-            'content': message.content,
-            'timestamp': str(message.timestamp)
-        }
-    commands = {
-        'fetch_messages': fetch_messages,
-        'new_message': new_message
-    }
-
+ 
+class ChatConsumer(JsonWebsocketConsumer):
+    """
+    This consumer is used to show user's online status,
+    and send notifications.
+    """
+ 
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+ 
     def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, 
-            self.channel_name
-            )
-
+        print("Connected!")
+        self.room_name = "home"
         self.accept()
-
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, 
-            self.channel_name
-            )
-            
-    # Receive message from WebSocket
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        self.commands[text_data_json['command']](self, text_data_json)
-    
-    def send_chat_message(self, message):
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, 
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name,
+        )
+        self.send_json(
             {
-                "type": "chat.message", 
-            "message": message
+                "type": "welcome_message",
+                "message": "Hey there! You've successfully connected!",
             }
         )
-
-    def send_message(self, message):
-        self.send(text_data=json.dumps(message))
-
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event["message"]
-        # Send message to WebSocket
-        self.send(text_data=json.dumps(message))
+     
+    def disconnect(self, code):
+        print("Disconnected!")
+        return super().disconnect(code)
+ 
+    def receive_json(self, content, **kwargs):
+        message_type = content["type"]
+        if message_type == "chat_message":
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_name,
+                {
+                    "type": "chat_message_echo",
+                    "name": content["name"],
+                    "message": content["message"],
+                }
+            )
+        return super().receive_json(content, **kwargs)
+    
+    def chat_message_echo(self, event):
+        print(event)
+        self.send_json(event)
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chatapp.urls', namespace='chatapp')),
+    path('', include('chatapp.urls', namespace='chatapp')),
     path('api/', include('chatapp_api.urls', namespace='chatapp_api'))
 ]
