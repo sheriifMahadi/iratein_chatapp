c74bc5831d44e6125bc0629bf9860550ed29d3b1
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-01-24 21:11:18 +0100

    django backend login

--- chatapp_api/views.py
+++ chatapp_api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import action
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.authtoken.views import ObtainAuthToken
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+
+class CustomObtainAuthTokenView(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        print("sending")
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key, "username": user.username})
