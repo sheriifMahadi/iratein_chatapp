9ab36839af8da6b0b50c267cab0d73f3efdb6d23
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-01-25 17:15:31 +0100

    deployment code

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -144,3 +144,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = ["http://localhost:5173"]
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
