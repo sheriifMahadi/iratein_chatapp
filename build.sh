80c37ec6c4a27ddbe6ab7f19077353b4125dd391
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-01-26 02:12:58 +0100

    deployment config

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 # exit on error
 set -o errexit
 
+python manage.py migrate
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations chatapp
-python manage.py makemigrations chatapp_api
+python manage.py makemigrations
 python manage.py migrate
 python manage.py createsu
