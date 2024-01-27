13cecf39dd6af30d8bddfd23ee4ea2c6d2b62dc3
sheriifMahadi <sheriffabdulamjid@gmail.com>
2024-01-27 04:06:21 +0100

    deployment config

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ cd /code
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsu
-python manage.py runworker channel_layer -v2
+# python manage.py runworker channel_layer -v2
 
 daphne -b -0.0.0.0 -p 8000 core.asgi:application 
 docker run -p 6379:6379 -d redis:5
